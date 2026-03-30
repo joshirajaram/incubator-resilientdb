251dd8b6f0f92225063c32567555532b3da2b339
joshirajaram <joshirajaram@users.noreply.github.com>
2026-03-30 01:22:19 +0000

    deploy: 4b3028501464836bbea6fe6a3d58e8c76209fa0e

---
+++ navtreedata.js
@@ -0,0 +1,92 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "ResilientDB", "index.html", [
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", "namespacemembers_vars" ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/ded/classresdb_1_1ConsensusManagerPBFT.html#accbe75d7be210c0357cde550d4947198",
+"d1/db2/classresdb_1_1ViewChangeManager.html#a458c26e26eb29aefd222f9d414799a48",
+"d3/d16/classResDBTxnAccessor.html#acad48a213c53f4477eb49c917ea16d2c",
+"d3/d97/classresilient__python__cache_1_1cache_1_1ResilientPythonCache.html#a2b0f57c15f255fc5ba00bb4be53adcfd",
+"d4/d4b/classKVServiceTransactionManager.html",
+"d5/d10/namespaceresdb.html#a9365b7af8267b084d545559cc0f87cb0a3279973d22efc152fd87dfe4f1710def",
+"d5/d57/classresdb_1_1contract_1_1MemoryDB.html#af1fb8cea5ed91abf154ce4fdb40f2a74",
+"d6/d84/namespaceutils.html",
+"d7/d95/classresdb_1_1TransactionCollector.html#a5204b67bb1f3c45644b998f88f6c9934",
+"d8/d6b/storage_8h_source.html",
+"d9/d68/classresdb__validator_1_1models_1_1Transaction.html",
+"da/d19/classresdb_1_1ConsensusManager.html#afddbd1de6fd7a2a629e593e879fc1b5e",
+"da/dcc/namespaceresdb__validator_1_1utils.html#a2ecf87921846e61a2a883d02e3395f24",
+"db/ddb/classresdb_1_1ResDBConfig.html#a3669f8ea7516a477902af0e357d47b84",
+"dc/d12/ecosystem_2graphql_2service_2kv__service_2kv__service__tools_8cpp.html#aae9011388f6fd7b287566f37e31660fa",
+"dc/db0/classresdb_1_1CheckPointManager.html#aceffced8931a5a2a9c6c4a517cfe489f",
+"dd/da8/namespaceadd__license__headers.html#a3fa65a16b4049a0018fbf3d1f1154a9b",
+"de/d46/classresdb_1_1BlockManager.html#a1e31337aabf7668a9fa5f874c74bd917",
+"de/dc3/classresdb_1_1NetChannel.html#aea266111b7b17de8da3c80de6fb38dd8",
+"df/dbe/classgraphql__client_1_1GraphQLClient.html#aab98ea24adeb718e8497ee832c32289c",
+"dir_d7cd2a92df2926f13a63ecd5966aa135.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
+var LISTOFALLMEMBERS = 'List of all members';
